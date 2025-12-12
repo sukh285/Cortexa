afd179059ee087d64224b5a742883b89ef0a9379
Sukhmeet Singh Lamba <130579680+sukh285@users.noreply.github.com>
2025-12-12 12:41:38 +0530

    allow user input from terminal

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import readline from "node:readline/promises";
+
+//The async fn returns a promise but it doesn't return any meaningful data
+async function main(): Promise<void> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  //for continuous chat between ai and user
+  while (true) {
+    const userInput = await rl.question("You: ");
+    console.log("You said:", userInput);
+    
+    if(userInput === 'exit') break;
+  }
+
+  //closes the interface
+  rl.close();
+}
+
+main();
